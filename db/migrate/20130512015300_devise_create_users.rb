c3f7d85bb562e83dcdcefda746958ca5a0b1b7b9
rails4sandeep <rails4sandeep@gmail.com>
2013-06-30 15:45:56 +1000

    changed add users migration again

--- db/migrate/20130512015300_devise_create_users.rb
+++ db/migrate/20130512015300_devise_create_users.rb
@@ -23,7 +23,7 @@ class DeviseCreateUsers < ActiveRecord::Migration
   # t.string :password_salt
 
   ## Confirmable
-  # t.string   :confirmation_token
+   t.string   :confirmation_token
   # t.datetime :confirmed_at
   # t.datetime :confirmation_sent_at
   # t.string   :unconfirmed_email # Only if using reconfirmable
