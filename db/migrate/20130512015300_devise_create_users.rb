6da290950235f6293cab146fff471008007eba59
rails4sandeep <rails4sandeep@gmail.com>
2013-06-30 15:32:49 +1000

    changed add users migration again

--- db/migrate/20130512015300_devise_create_users.rb
+++ db/migrate/20130512015300_devise_create_users.rb
@@ -1,4 +1,4 @@
-create_table(TABLE_NAME) do |t|
+create_table(:users) do |t|
   ## Database authenticatable
   t.string :email,              :null => false, :default => ""
   t.string :encrypted_password, :null => false, :default => ""
